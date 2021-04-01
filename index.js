ae498b36dc4f8b94edc7adb175a4b896ef665879
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-31 20:26:34 -0400

    Setting up EJS

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.set('view-enginer', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
 app.listen(8080);
