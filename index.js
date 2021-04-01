eae3d2bcdad299c56e55656bd4c06ca0de41b022
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-31 20:34:23 -0400

    Added login and register routes and views

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const app = express();
 
 app.set('view-enginer', 'ejs');
 
+
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
 });
 
 app.listen(8080);
