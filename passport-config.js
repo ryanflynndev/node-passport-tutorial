bca3c8189d833919f68206cb301ac96abca43475
Ryan Flynn <ryanflynn44@gmail.com>
2021-04-03 15:05:11 -0400

    Added error handling

--- passport-config.js
+++ passport-config.js
@@ -1,9 +1,14 @@
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 
 
 function initialize(passport){
     const authenticateUser = (email, password, done) => {
         const user = getUserByEmail(email);
+    }
+
+    if(user === null){
+        return done(null, false, { message: "No user with that email" });
     }
     passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser));
     passport.serializeUser((user, done) => {
