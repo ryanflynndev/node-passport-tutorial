8329a6e2bbf37ca00de9de96a05ab6dfed35adc5
Ryan Flynn <ryanflynn44@gmail.com>
2021-04-04 20:31:15 -0400

    Logic for authenticating user by password

--- passport-config.js
+++ passport-config.js
@@ -10,6 +10,21 @@ function initialize(passport){
     if(user === null){
         return done(null, false, { message: "No user with that email" });
     }
+
+    try {
+        if(await bcrypt.compare(password, user.password)){
+            //authenticated user
+            return done(null, user);
+        } else {
+            return done(null, false, {message: 'Password Incorrect'});
+        }
+    } catch {
+        return done(e);
+
+    }
+
+
+
     passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser));
     passport.serializeUser((user, done) => {
 
